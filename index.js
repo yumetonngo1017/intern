620fcbcbb142e9aba6c132e86985a5d1897cf5c9
intern004 <intern004@50-64722.local>
2018-03-12 18:53:14 +0900

    intern

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+fetch('https://api.fixer.io/latest?base=USD', {
+  method: 'GET' // or 'PUT'
+}).then(res => res.json())
+.catch(error => console.error('Error:', error))
+.then(response => {
+  // console.log('Success:', response)
+   // document.write('今の日本円 -> ' + response.rates.JPY);
+   var yenElem  = document.getElementById("yen");
+   yenElem.innerHTML = response.rates.JPY;
+});
