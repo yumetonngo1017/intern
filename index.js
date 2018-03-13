5f556fe8eaf05ad84f5a94a5fe8da659e9693178
intern004 <intern004@50-64722.local>
2018-03-13 18:45:56 +0900

    design

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
+// submitボタンのDOM取得する
 var submitBtn = document.getElementById("submit");
+// クリックイベントを登録する
 submitBtn.addEventListener("click", onButtonClick);
-
+//クッリクしたあとの処理
 function onButtonClick() {
-  const search = document.form1.textBox1.value;;
-  fetch('https://api.github.com/search/repositories?q='+search, {
+  //検索内容の取得
+  const search = document.retrieval.textBox.value;
+  fetch('https://api.github.com/search/repositories?q=' + search + '&sort=stars&order=desc', {
     method: 'GET' // or 'PUT'
   }).then(res => res.json())
-  .catch(error => console.error('Error:', error))
   .then(response => {
-    // console.log('Success:', response)
-    const yenElem  = document.getElementById("yen");
-     // document.write('今の日本円 -> ' + response.rates.JPY);
-     //onsole.log(JSON.stringify(yenElem));
+    const contantElem  = document.getElementById("contant");
     let tags = '';
     for (var i = 0; i < response.items.length; i++) {
-      tags += '<div class="name">' + response.items[i].name + '</div>' + '<div class="url">' + response.items[i].clone_url + '</div>';
+      /*tags += '<div class="name">'+ 'NAME : '  + response.items[i].name +  '</div>' + '<div class="star">' + '⭐️ : ' + response.items[i].stargazers_count + '</div>'
+      + '</div>' + '<div class="url">'+ 'URL : '  + response.items[i].url + '</div>';
+      */
+      tags += '<div class="name">'+ 'NAME : '  + response.items[i].name +  '</div>' + '<div class="star">' + '⭐️ : ' + response.items[i].stargazers_count + '</div>'
+      + '</div>' + `<a href="${response.items[i].clone_url}" class="url">`+ response.items[i].clone_url + '</a>';
     }
-     //debugger
-    yenElem.innerHTML = tags;
-     //https://api.fixer.io/latest?base=USD
+    contantElem.innerHTML = tags;
   });
 }
-//target = 'intern';
 
 
 
+//お助けコマンド
 //debugger
