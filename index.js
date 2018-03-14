94db3593c76aa6916e4c44f0c8bfe8bce8ffa315
intern004 <intern004@50-64722.local>
2018-03-14 13:35:33 +0900

    Update css style

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 // submitボタンのDOM取得する
-var submitBtn = document.getElementById("submit");
+const submitBtn = document.getElementById("submit");
+
 // クリックイベントを登録する
 submitBtn.addEventListener("click", onButtonClick);
+
 //クッリクしたあとの処理
 function onButtonClick() {
   //検索内容の取得
   const search = document.retrieval.textBox.value;
   fetch('https://api.github.com/search/repositories?q=' + search + '&sort=stars&order=desc', {
-    method: 'GET' // or 'PUT'
+    method: 'GET'
   }).then(res => res.json())
+  .catch(error => console.error('Error:', error))
   .then(response => {
     const contantElem  = document.getElementById("contant");
     let tags = '';
     for (var i = 0; i < response.items.length; i++) {
-      /*tags += '<div class="name">'+ 'NAME : '  + response.items[i].name +  '</div>' + '<div class="star">' + '⭐️ : ' + response.items[i].stargazers_count + '</div>'
-      + '</div>' + '<div class="url">'+ 'URL : '  + response.items[i].url + '</div>';
-      */
+
       tags += '<div class="name">'+ 'NAME : '  + response.items[i].name +  '</div>' + '<div class="star">' + '⭐️ : ' + response.items[i].stargazers_count + '</div>'
       + '</div>' + `<a href="${response.items[i].clone_url}" class="url">`+ response.items[i].clone_url + '</a>';
+
+      contantElem.innerHTML = tags;
     }
-    contantElem.innerHTML = tags;
   });
 }
-
-
 
 //お助けコマンド
 //debugger
